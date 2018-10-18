426f4671923e86433ea95f4611e0104979688c78
Michal Mrzena <michal.mrzena@gmail.com>
2018-10-18 14:43:07 +0200

    Updates

---
+++ precache-manifest.bf7b97c08c2d74f5d2da02494fdcd1eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9015724e9ee56b879a6a788ddd78e758",
+    "url": "/pocasi/static/media/clouds1.9015724e.png"
+  },
+  {
+    "revision": "427e34bbbcdde154657e",
+    "url": "/pocasi/static/js/runtime~main.427e34bb.js"
+  },
+  {
+    "revision": "fd111eb483beb59f8490",
+    "url": "/pocasi/static/js/main.fd111eb4.chunk.js"
+  },
+  {
+    "revision": "5fb649a034f9ba5f6b5c",
+    "url": "/pocasi/static/js/1.5fb649a0.chunk.js"
+  },
+  {
+    "revision": "fd111eb483beb59f8490",
+    "url": "/pocasi/static/css/main.07e32843.chunk.css"
+  },
+  {
+    "revision": "d1834b3e4ff84dc302486a360d493f40",
+    "url": "/pocasi/index.html"
+  }
+];
